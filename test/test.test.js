df6aef29af49049fb40ffc1e40fcebc60cd89d0f
davebhavin9 <dave.b@northeastern.edu>
2020-11-05 09:47:53 -0500

    trying to debug script files

--- test/test.test.js
+++ test/test.test.js
@@ -3,12 +3,14 @@ const app = require('../server/index.js');
 
 
 describe('GET check server', () => {
+    
     it("checking server is online or not", async() => {
+        jest.setTimeout(3000);
         let result = await request(app)
             .get('/check')
+            //.set('Authorization', 'Basic '+new Buffer.from("test@test.com:Test@1234").toString("base64"))
             .expect(200)
-        if(!result) return false;
+    
     });
-
 });
 
