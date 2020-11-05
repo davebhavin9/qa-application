5c214804e3828f53d09a26d98d7733bbe7c4596c
davebhavin9 <dave.b@northeastern.edu>
2020-11-05 09:20:38 -0500

    trying to debug script files

--- test/test.test.js
+++ test/test.test.js
@@ -1,21 +1,14 @@
 const request = require('supertest');
-const app = require('../server/index');
+const app = require('../server/index.js');
 
 
-describe('GET /checkAuthorization', () => {
-    it("should authorize the user", async() => {
+describe('GET check server', () => {
+    it("checking server is online or not", async() => {
         let result = await request(app)
-            .get('/v1/user/checkAuthorization')
-            .set('Authorization', 'Basic '+new Buffer.from("test@test.com:Test@1234").toString("base64"))
+            .get('/check')
             .expect(200)
         if(!result) return false;
     });
 
-    it("should not authorize the user", async() => {
-        let result = await request(app)
-            .get('/v1/user/checkAuthorization')
-            .set('Authorization', 'Basic '+new Buffer.from("test@test.com:Test@1234"))
-            .expect(401)
-        if(!result) return false;
-    });
 });
+
