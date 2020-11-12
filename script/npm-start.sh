5e1e1f566f595eae8e66b5164483ff90221e43eb
davebhavin9 <dave.b@northeastern.edu>
2020-11-11 20:22:14 -0500

    logger and sdc

--- script/npm-start.sh
+++ script/npm-start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/
 npm install
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:cloudwatch-agent-config.json -s
 sudo nohup node server >> output.log 2>&1 &
